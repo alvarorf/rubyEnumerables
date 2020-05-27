d7144fab3cf26e68ac2288818c2dedf6f5bbd918
alvaro <aaruizf@unal.edu.co>
2020-05-27 16:11:40 -0400

    Added my_inject method

--- main.rb
+++ main.rb
@@ -94,7 +94,23 @@ def my_map(my_proc = UNDEFINED)
   result
 end
 
-def my_inject (m)
-  return m
+def my_inject(*arg)
+  new_arr = to_a
+  accumulator = arg[0]
+  if arg[0].class == Symbol
+    accumulator = new_arr[0]
+    new_arr = new_arr[1..-1]
+    new_arr.my_each { |x| accumulator = accumulator.send(arg[0], x) }
+  elsif arg[0].class < Numeric && arg[1].class != Symbol
+    new_arr.my_each { |x| accumulator = yield(accumulator, x) }
+  elsif arg[0].class < Numeric && arg[1].class == Symbol
+    new_arr.my_each { |x| accumulator = accumulator.send(arg[1], x) }
+  else
+    accumulator = new_arr[0]
+    new_arr = new_arr[1..-1]
+    new_arr.my_each { |x| accumulator = yield(accumulator, x) }
+  end
+  accumulator
 end
+  
     
