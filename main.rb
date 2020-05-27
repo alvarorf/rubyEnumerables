f3b7f4012a64b85adf8aaaca4fe9ff64dfd9ffb8
alvaro <aaruizf@unal.edu.co>
2020-05-27 15:59:31 -0400

    Added my_all? my_any? and my_none? methods

--- main.rb
+++ main.rb
@@ -32,16 +32,34 @@ def my_select
   result
 end
 
-def my_all? (m)
-  return m
+def my_all?(arg = UNDEFINED)
+  unless block_given?
+    if arg != UNDEFINED
+      my_each { |x| return false unless arg === x }
+    else
+      my_each { |x| return false unless x }
+    end
+    return true
+  end
+  my_each { |i| return false unless yield(i) }
+  true
 end
 
-def my_any? (m)
-  return m
+def my_any?(arg = UNDEFINED, &block)
+  unless block_given?
+    if arg != UNDEFINED
+      my_each { |x| return true if arg === x }
+    else
+      my_each { |x| return true if x }
+    end
+    return false
+  end
+  my_each { |i| return true if block.call(i) }
+  false
 end
 
-def my_none? (m)
-  return m
+def my_none?(arg = UNDEFINED, &block)
+  !my_any?(arg, &block)
 end
 
 def my_count (m)
